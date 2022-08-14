376aecd689c9e01fb7288fa0bc3cc58248a0f0d6
summer-MQ <mq1009416078@163.com>
2022-08-14 20:49:39 +0800

    homework完成

--- src/homework/index.vue
+++ src/homework/index.vue
@@ -5,8 +5,20 @@
       <span>第{{ page }}/{{ totalPage }}页</span>
     </div>
     <div class="right">
-      <button :disabled="page === 1" @click="changeDate(0)">上一页</button>
-      <button :disabled="page === totalPage" @click="changeDate(1)">下一页</button>
+      <button
+        :disabled="page === 1"
+        :class="{
+          forbid:page === 1
+        }"
+        @click="changeDate(0)"
+      >上一页</button>
+      <button
+        :disabled="page === totalPage"
+        :class="{
+          forbid:page === totalPage
+        }"
+        @click="changeDate(1)"
+      >下一页</button>
     </div>
   </div>
 </template>
@@ -69,8 +81,22 @@ export default {
 .left span{
   display: inline-block;
   margin-right: 12px;
+  color: #dbdfe5;
+  font-weight: 400;
+  font-size: 16px
 }
 .right button {
-  margin-left: 12px;
+  /* margin-left: 12px; */
+  background-color: #d5ddf8;
+  font-size: 13px;
+  border: none;
+  width: 70px;
+  height: 32px;
+  margin: 0 16px;
+  border-radius: 2px;
+}
+.forbid{
+  cursor: not-allowed;
+  background-color: #edf0f9;
 }
 </style>
