e48a71b4d6b3d5a6ee02d0702b5aad21924cbe9e
summer-MQ <mq1009416078@163.com>
2022-08-14 20:39:25 +0800

    已完成

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,33 @@
 <template>
   <div id="app">
-    <!-- <router-view /> -->
+    <router-view />
     <!-- <homework :total="50" :num="5" @preDate="preDate" @nextDate="nextDate" @pageChange="request" /> -->
-    <homework :total="50" :num="5" @preDate="preDate" @nextDate="nextDate" />
+    <!-- <homework :total="50" :num="5" @preDate="preDate" @nextDate="nextDate" /> -->
   </div>
 </template>
 
 <script>
-import homework from './homework'
+// import homework from './homework'
 export default {
-  name: 'App',
-  components: {
-    homework
-  },
-  data() {
-    return {
-      page: 1
-    }
-  },
-  methods: {
-    preDate() {
-      console.log('上一页数据')
-    },
-    nextDate() {
-      console.log('下一页数据')
-    },
-    request(page) {
-      console.log('发送请求', page)
-    }
-  }
+  name: 'App'
+  // components: {
+  //   homework
+  // },
+  // data() {
+  //   return {
+  //     page: 1
+  //   }
+  // },
+  // methods: {
+  //   preDate() {
+  //     console.log('上一页数据')
+  //   },
+  //   nextDate() {
+  //     console.log('下一页数据')
+  //   },
+  //   request(page) {
+  //     console.log('发送请求', page)
+  //   }
+  // }
 }
 </script>
